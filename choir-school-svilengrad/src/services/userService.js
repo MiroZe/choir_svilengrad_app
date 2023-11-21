ea44dec062de95e5f6a41ef58a1a716bac4925f2
MiroZe <miro_sv@yahoo.com>
2023-11-21 14:57:34 +0200

    reafactoring, using requester

--- choir-school-svilengrad/src/services/userService.js
+++ choir-school-svilengrad/src/services/userService.js
@@ -1,22 +1,27 @@
-
+import {request} from './requester'
 const baseURL = 'http://localhost:3000';
 
-const headers =  {"Content-Type": "application/json",}
+
 
 export const userRegister = async (userData) => {
 
-    const response = await fetch(`${baseURL}/register`, {method:'POST', headers:headers, credentials: 'include', body:JSON.stringify(userData) })
-    const result = await response.json();
+    const result = request.post(`${baseURL}/register`,userData) 
+  
     return result;
 }
+
+
 
 export const userLogin = async (userData) => {
-    const response = await fetch(`${baseURL}/login`, {method:'POST', credentials: 'include',headers:headers, body:JSON.stringify(userData) })
-    const result = await response.json();
-    return result;
+
+    const result = await request.post(`${baseURL}/login`,userData );
+  
+return result
+
 }
 
+
+
 export const userLogout = async () => {
-    return await fetch(`${baseURL}/logout`, {method:'POST',credentials:'include'});
-   
+         return await request.post(`${baseURL}/logout`)
 }
